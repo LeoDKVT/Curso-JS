fa6b3f09f5151730bb83b69c37d44bcf06beb3c2
José Leonardo <leodkvt@gmail.com>
2022-05-19 09:50:44 -0300

    atl

--- aula12exe/exe014/script.js
+++ aula12exe/exe014/script.js
@@ -1,10 +1,22 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var imagem = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
 
-var msg = window.document.getElementById(`msg`)
-var imagem = window.document.getElementById(`imagem`)
-var data = new Date()
-var hora = data.getHours()
-
-msg.innerHTML = `Agora são ${hora} horas`
-if (hora >= 0  && hora < 12) {
-    
+    msg.innerHTML = `Agora são ${hora}:${minuto}`
+    if (hora >= 0  && hora < 12) {
+          //Bom dia!
+          imagem.src = 'manha.png'
+         document.body.style.backgroundColor = `#F0DD9C`
+    } else if (hora >=12 && hora <= 17) {
+          //Boa tarde!
+         imagem.src = `tarde.png`
+         document.body.style.backgroundColor = `#BF7E78`
+    } else {
+        // Boa noite!
+        imagem.src = 'noite.png'
+        document.body.style.backgroundColor = `#45443E`
+    }
 }
